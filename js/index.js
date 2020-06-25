45f4949bdf33ec2ebc47165619300a798f610355
HadesVsPluto <tibor.pluto@googlemail.com>
2020-06-25 13:21:11 +0200

    index page style chosen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
+//
+// $(window).on('resize', function(){
+//   $(".image_col").height($(window).height())
+// });
+// $(window).on('load', function(){
+//   $(".image_col").height($(window).height())
+// });
 
-$(window).on('resize', function(){
-  $(".image_col").height($(window).height())
-});
-$(window).on('load', function(){
-  $(".image_col").height($(window).height())
-});
+
+// jQuery.fn.center = function () {
+//     this.css("position","absolute");
+//     this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+//                                                 $(window).scrollTop()) + "px");
+//     this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+//                                                 $(window).scrollLeft()) + "px");
+//     return this;
+// }
+//
+//
+// $(window).on('load', function(){
+//   $("txt_maintitle").center());
+// });
+// $(window).on('resize', function(){
+//   $("txt_maintitle").center())
+// });
